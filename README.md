242f4a9badf5da5921b088944f0a280cd5e3cdeb
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-03-02 15:54:01 -0300

    README.md

--- README.md
+++ README.md
@@ -1,34 +1,16 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+### Portfólio
 
-## Getting Started
+Portfólio pessoal com projetos que desenvolvi durante minha trajetória como desenvolvedor
 
-First, run the development server:
+![](./public/design/desktop-preview.jpg)
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+O site possui uma página inicial e uma página de projetos <br /><br />
+Para a página inicial foi utilizado a função <b><i>getStaticProps</i></b> do Next.js para obter os projetos fixados e tecnologias fornecidos pelo DatoCMS <br /><br />
+Para a página de projetos foram utilizadas as funções <b><i>getStaticProps</i></b> e <b><i>getStaticPaths</i></b> do Next.js para obter todos os projetos fornecidos pelo DatoCMS e assim criar a paginação e os filtros de projetos por tecnologia
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### Tecnologias
+- Next.js
+- TypeScript
+- SASS
+- DatoCMS
+- AOS
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,40 +10,7 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="/assets/favicon.svg" />
-          <meta charSet="utf-8" />
           <meta name="theme-color" content='#6673d7' />
-
-          <meta name="application-name" content="Portfólio - Luiz Oliveira" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="Portfólio - Luiz Oliveira" />
-          <meta name="description" content="Portfólio pessoal com projetos que desenvolvi durante minha trajetória como desenvolvedor" />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-TileColor" content="#556BD1" />
-          <meta name="msapplication-tap-highlight" content="no" />
-
-          <link rel="apple-touch-icon" href="/assets/favicon.svg" />
-
-          <meta property="og:title" content="Portfólio - Luiz Oliveira" />
-          <meta property="og:description" content="Portfólio pessoal com projetos que desenvolvi durante minha trajetória como desenvolvedor" />
-          <meta property="og:locale" content="pt_BR" />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="Portfólio - Luiz Oliveira" />
-          <meta property="og:image" content="/assets/ogimage.jpg" />
-          <meta property="og:image:secure_url" content="/assets/ogimage.jpg" />
-          <meta property="og:image:alt" content="Thumbnail" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="900" />
-          <meta property="og:image:height" content="600" />
-
-          <meta name="twitter:title" content="Portfólio - Luiz Oliveira" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:image" content="/assets/ogimage.jpg" />
-          <meta name="twitter:image:src" content="/assets/ogimage.jpg" />
-          <meta name="twitter:image:alt" content="Thumbnail" />
-          <meta name="twitter:image:width" content="900" />
-          <meta name="twitter:image:height" content="600" />
         </Head>
 
         <body>
