f571e5bb97e10f4270587aaaf9fa8598bc51d4e8
Luiz Oliveira <luizoliveira2808@gmail.com>
2024-11-06 16:37:14 -0300

    styles: transition

--- src/types.ts
+++ src/types.ts
@@ -4,8 +4,8 @@ export type Technology = {
   slug: string;
   logo: {
     url: string;
-  }
-}
+  };
+};
 
 export type Project = {
   id: string;
@@ -17,44 +17,44 @@ export type Project = {
   thumbnail: {
     url: string;
   };
-}
+};
 
 export type Curriculum = {
   file: {
     url: string;
-  }
-}
+  };
+};
 
 export type About = {
   description: string;
-}
+};
 
 export type GetLatestProjectsResponse = {
   allProjects: Array<Project>;
-}
+};
 
 export type GetTechnologiesResponse = {
   allTechnologies: Array<Technology>;
-}
+};
 
 export type GetTechnologyResponse = {
   technology: Technology;
-}
+};
 
 export type CountProjectsResponse = {
   _allProjectsMeta: {
     count: number;
-  }
-}
+  };
+};
 
 export type GetProjectsResponse = {
   allProjects: Array<Project>;
-}
+};
 
 export type GetCurriculumResponse = {
   curriculum: Curriculum;
-}
+};
 
 export type GetAboutResponse = {
   about: About;
-}
+};
