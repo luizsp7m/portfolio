0df36c13eaac6cfadbcb8bf5b8670325e7553243
Luiz Oliveira <luizoliveira2808@gmail.com>
2024-11-07 11:57:15 -0300

    refactor: technology list

--- src/components/ProjectList/components/ProjectCard/index.tsx
+++ src/components/ProjectList/components/ProjectCard/index.tsx
@@ -6,12 +6,24 @@ import clsx from "clsx";
 
 import { FaGithub } from "react-icons/fa";
 import { Project } from "../../../../types";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from "react";
 
 interface Props {
   project: Project;
 }
 
 export function ProjectCard({ project }: Props) {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  function handleOpenDrawer() {
+    setDrawerIsOpen(true);
+  }
+
+  function handleCloseDrawer() {
+    setDrawerIsOpen(false);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles["project-image"]}>
@@ -47,7 +59,7 @@ export function ProjectCard({ project }: Props) {
                   href={`/projetos/${technology.slug}/1`}
                 >
                   <a
-                    className={clsx({
+                    className={clsx(styles["technology-link"], {
                       [styles.width]: project.technologies.length > 3,
                     })}
                   >
@@ -58,25 +70,32 @@ export function ProjectCard({ project }: Props) {
           )}
 
           {project.technologies.length > 3 && (
-            <span>
+            <button onClick={handleOpenDrawer}>
               + {project.technologies.length - 3}
-              <div className={styles["popover-overlay"]}>
-                <div className={styles.popover}>
-                  {project.technologies.map(
-                    (technology, index) =>
-                      index >= 3 && (
-                        <Link
-                          key={technology.id}
-                          href={`/projetos/${technology.slug}/1`}
-                        >
-                          <a>{technology.name}</a>
-                        </Link>
-                      )
-                  )}
-                </div>
-              </div>
-            </span>
+            </button>
           )}
+        </div>
+
+        <div
+          className={clsx(styles["technologies-drawer"], {
+            [styles["show-drawer"]]: drawerIsOpen,
+          })}
+        >
+          <div className={styles["technologies-drawer-header"]}>
+            <h5>Tecnologias: </h5>
+
+            <button onClick={handleCloseDrawer}>
+              <IoCloseSharp size={16} />
+            </button>
+          </div>
+
+          <div className={styles["technologies-drawer-body"]}>
+            {project.technologies.map((technology) => (
+              <Link key={technology.id} href={`/projetos/${technology.slug}/1`}>
+                <a className={styles["technology-link"]}>{technology.name}</a>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
