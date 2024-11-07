93367cda0c291bda6dabeef7cec26bb8f073e8f4
Luiz Oliveira <luizoliveira2808@gmail.com>
2024-11-07 19:21:06 -0300

    feat: remove theme from cookies

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,6 +1,4 @@
-import Cookies from "js-cookie";
-
-import { createContext, ReactNode, useLayoutEffect, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 interface ThemeContextProps {
   isLightTheme: boolean;
@@ -8,33 +6,23 @@ interface ThemeContextProps {
 }
 
 interface ThemeProviderProps {
-  defaultTheme: "dark" | "light";
   children: ReactNode;
 }
 
 export const ThemeContext = createContext({} as ThemeContextProps);
 
-export function ThemeProvider({ defaultTheme, children }: ThemeProviderProps) {
-  const [currentTheme, setCurrentTheme] = useState<"dark" | "light">(
-    defaultTheme
-  );
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [isLightTheme, setIsLightTheme] = useState(false);
 
   function handleToggleLightTheme() {
-    if (currentTheme === "dark") {
-      Cookies.set("theme", "light");
-      document.body.classList.add("light-theme");
-      setCurrentTheme("light");
-    } else {
-      Cookies.set("theme", "dark");
-      document.body.classList.remove("light-theme");
-      setCurrentTheme("dark");
-    }
+    setIsLightTheme((prevState) => !prevState);
+    document.body.classList.toggle("light-theme");
   }
 
   return (
     <ThemeContext.Provider
       value={{
-        isLightTheme: currentTheme === "light",
+        isLightTheme,
         handleToggleLightTheme,
       }}
     >
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,17 @@
 import "../styles/global.scss";
 
-import App, { AppContext, AppInitialProps, AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../services/apollo";
 import { ThemeProvider } from "../contexts/ThemeContext";
 
-function MyApp({ Component, pageProps, theme }) {
-  if (typeof window !== "undefined") {
-    if (theme === "light") {
-      document.body.classList.add("light-theme");
-    }
-  }
-
+function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider defaultTheme={theme}>
+      <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
   );
 }
-
-interface AppOwnProps {
-  theme: "light" | "dark";
-}
-
-MyApp.getInitialProps = async (
-  context: AppContext
-): Promise<AppOwnProps & AppInitialProps> => {
-  let theme: "dark" | "light" = "dark";
-
-  const ctx = await App.getInitialProps(context);
-
-  if (context.ctx.req) {
-    const cookies = context.ctx.req.headers.cookie;
-    theme = cookies?.includes("theme=light") ? "light" : "dark";
-  }
-
-  return { ...ctx, theme };
-};
 
 export default MyApp;
